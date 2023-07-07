cad581f8e2c7e2024207f2eaf1a5a2367d9b7ffd
unknown <lukeharriman1013@gmail.com>
2023-07-06 18:11:00 -0700

    added some questions for later

---
+++ script.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+      question: "What is JavaScript?",
+      options: {
+        a: "A markup language",
+        b: "A programming language",
+        c: "A styling language",
+        d: "A database language"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which keyword is used to declare a variable in JavaScript?",
+      options: {
+        a: "let",
+        b: "var",
+        c: "const",
+        d: "both a and b"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is the output of the following code? console.log(2 + '2');",
+      options: {
+        a: "4",
+        b: "22",
+        c: "undefined",
+        d: "NaN"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which of the following is NOT a JavaScript data type?",
+      options: {
+        a: "String",
+        b: "Number",
+        c: "Boolean",
+        d: "Float"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What does the DOM stand for?",
+      options: {
+        a: "Document Object Model",
+        b: "Data Object Model",
+        c: "Dynamic Object Model",
+        d: "Document Order Model"
+      },
+      correctAnswer: "a"
+    }
+  ];
